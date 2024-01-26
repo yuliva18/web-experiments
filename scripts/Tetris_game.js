1101239a32b8b8a6b70ced5d43da6f06b85ba18e
Памагити <129578156+yuliva18@users.noreply.github.com>
2024-01-26 03:16:16 +0300

    Tetris works

--- scripts/Tetris_game.js
+++ scripts/Tetris_game.js
@@ -10,13 +10,53 @@ canvas.width  = xSize * cellSize;
 ctx.fillStyle = "orange";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-let cur_x = 5;
+let allFigures = [
+    [
+        [1, 1],
+        [1, 1],
+    ],
+    [
+        [0,1,0],
+        [1,1,1],
+        [0,0,0]
+    ],
+    [
+        [0,1,0],
+        [0,1,0],
+        [0,1,1]
+    ],
+    [
+        [0,1,0],
+        [0,1,0],
+        [1,1,0]
+    ],
+    [
+        [0,0,0],
+        [0,1,1],
+        [1,1,0]
+    ],
+    [
+        [0,0,0],
+        [1,1,0],
+        [0,1,1]
+    ],
+    [
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0]
+    ]
+]
+
+let cur_x = 4;
 let cur_y = 0;
+let cur_figure = null;
 let blocks = new Array(ySize);
 for (let i=0;i<ySize;i++){
     blocks[i] = new Array(xSize);
     blocks[i].fill(0);
 }
+createFigure(cur_x, cur_y);
 //Настройка отрисовки
 let oldTime = null;
 let newTime = null;
@@ -35,11 +75,13 @@ let step = 0;
 function update(){
     clear();
     //Умирать
-    if (step===5){
+    if (step===25){
         if (moveCurrent(0, 1)) null;
         else {
-            blocks[cur_y][cur_x] = 1;
-            if (createFigure(5, 0)) null;
+            stopFigure();
+            cur_x = 4;
+            cur_y = 0;
+            if (createFigure(cur_x, cur_y)) null;
             else {
                 updateBool = false;
                 console.log("lose");
@@ -50,12 +92,17 @@ function update(){
                 return;
             }
         }
+        removeLines();
         step=0;
     }
     step++;
     //Рисовать
     ctx.fillStyle = "black";
-    ctx.fillRect(cellSize * cur_x, cellSize * cur_y, cellSize, cellSize);
+    for (let i=0;i<cur_figure.length;i++){
+        for (let j=0;j<cur_figure[i].length;j++){
+            if (cur_figure[i][j]===1) ctx.fillRect(cellSize * (cur_x + j), cellSize * (cur_y + i), cellSize, cellSize);
+        }
+    }
     ctx.fillStyle = "grey";
     for (let i=0;i<blocks.length;i++){
         for (let j=0;j<blocks[i].length;j++){
@@ -64,24 +111,73 @@ function update(){
     }
 }
 
-function moveCurrent(x, y){
-    try {
-        if (blocks[cur_y+y][cur_x+x]===0) {
-            cur_y+=y;
-            cur_x+=x;
-            return true;
+//Фигуру можно поставить
+function figureIsReal(figure,x,y){
+    for (let i=0;i<figure.length;i++){
+        for (let j=0;j<figure[i].length;j++){
+            if (figure[i][j]==1 && blocks[y+i]===undefined) return false;
+            if (figure[i][j]==1 && (blocks[y+i][x+j]===1 || blocks[y+i][x+j]===undefined)) return false;
         }
     }
-    catch{return false;}
+    return true;
 }
-function createFigure(x, y){
-    if (blocks[y][x]!=1) {
-        cur_y=y;
-        cur_x=x;
+//Сдвиг фигуры
+function moveCurrent(x, y){
+    if (figureIsReal(cur_figure, cur_x+x, cur_y+y)){
+        cur_x+=x;
+        cur_y+=y;
+        console.log(cur_x, cur_y);
         return true;
     }
     return false;
 }
+//Появление фигуры
+function createFigure(x, y){
+    let figureIndex = Math.floor(Math.random() * allFigures.length);
+    let figure = allFigures[figureIndex];
+    if (figureIsReal(figure, x, y)){
+        cur_figure = figure;
+        return true;
+    }
+    return false;
+}
+//Полная остановка фигуры
+function stopFigure(){
+    for (let i=0;i<cur_figure.length;i++){
+        for (let j=0;j<cur_figure[i].length;j++){
+            if (cur_figure[i][j]==1) blocks[cur_y+i][cur_x+j]=1;
+        }
+    }
+}
+//Повород фигуры
+function rotateFigure(right = true){
+    let newFigure = new Array(cur_figure.length);
+    for (let i=0;i<cur_figure.length;i++){
+        newFigure[i] = new Array(cur_figure[i].length);
+        newFigure[i].fill(0);
+        if (right){
+            for (let j=0;j<cur_figure[0].length;j++){
+                newFigure[i][j]=cur_figure[j][cur_figure[0].length-i-1];
+            }
+        }
+    }
+    if (figureIsReal(newFigure,cur_x,cur_y))cur_figure = newFigure;
+}
+//Удаление заполненных строк
+function removeLines(){
+    for (let i=blocks.length-1;i>=0;i--){
+        let doRemove = true;
+        for (let j=0;j<blocks[i].length;j++){
+            if(blocks[i][j]!=1)doRemove=false;
+        }
+        if (doRemove){
+            blocks.splice(i, 1);
+            blocks = [new Array(xSize)].concat(blocks);
+            blocks[0].fill(0);
+        }
+    }
+}
+//Очистка массива блоков
 function blocksClear(){
     for (let i=0;i<ySize;i++){
         blocks[i].fill(0);
@@ -90,12 +186,13 @@ function blocksClear(){
 
 //Обработка нажатий клавиш
 window.addEventListener("keydown", (event) => {
-    // console.log(event.key, event.keyCode);
+    console.log(event.key, event.keyCode);
     switch(event.keyCode){
         case 32: updateBool=!updateBool; break;
         case 83: if (updateBool) moveCurrent(0, 1); break;
         case 65: if (updateBool) moveCurrent(-1, 0); break;
         case 68: if (updateBool) moveCurrent(1, 0); break;
+        case 87: if (updateBool) rotateFigure(); break;
         default: break;
     }
     return;
